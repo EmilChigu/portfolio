3f6a3f935cd29c11a8d6bbf2da4a18ca267f9f4e
Emil <emil.chigu@gmail.com>
2022-06-08 17:29:26 +0100

    first commit

--- pages/components/HeadComponent.jsx
+++ pages/components/HeadComponent.jsx
@@ -16,7 +16,7 @@ function HeadComponent() {
 				name="description"
 			/>
 			<link rel="preconnect" href="https://fonts.googleapis.com" />
-			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
 			<link
 				href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;1,100&family=Open+Sans:wght@300&display=swap"
 				rel="stylesheet"
